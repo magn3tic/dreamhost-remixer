c4ca9f181f0f445cf46bb25ce1ae1ce64db622ae
Colin McGraw <colinmcgraw@Colins-MacBook-Pro.local>
2017-06-06 12:34:22 -0700

    header js - few lines got deleted fix

--- _js/app.js
+++ _js/app.js
@@ -1,7 +1,3 @@
-import $ from 'jquery';
-import Flickity from 'flickity';
-import $f from './sliders';
-
 //plugins.min.js is loaded before the webpack bundle
 //it is a bundle of jquery & plugins because some don't yet support es6 module
 
@@ -116,7 +112,7 @@ const ticker = () => {
 
 ticker.call();
 $window.resize(scrollUpdate);
-
+$window.scroll(() => didScroll = true);
 
 
 
